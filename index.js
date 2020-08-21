00a537cd812b2f91222f4f75faef7a185ac20bbe
Andy Burkovetsky <jamandyjam@gmail.com>
2020-08-21 12:18:04 +0300

    Add route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/users", (req, res) => {
+  res.send("123 Users online!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
